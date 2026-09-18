a1982593243348abe28981a45b982f7faee71601
PufferPanel Marketplace Installer <installer@local>
2026-09-18 10:00:44 +0800

    Add pagination and next page button in mod list

---
+++ web/js/Tasks-DFiwh6GB.js
@@ -0,0 +1,2 @@
+import{c as e,o as t}from"./index-6aFRqPP2.js";const c={__name:"Tasks",props:{server:{type:Object,required:!0}},setup(r){return(s,o)=>(t(),e("div",null," Tasks not yet implemented "))}};export{c as default};
+//# sourceMappingURL=Tasks-DFiwh6GB.js.map
