7a90bf290fafa936360a159f1bc8ac182449a1dc
PufferPanel Marketplace Installer <installer@local>
2026-09-18 09:56:17 +0800

    Auto-detect and install required mod dependencies with re-download prevention

---
+++ web/js/Tasks-CGogD322.js
@@ -0,0 +1,2 @@
+import{c as e,o as t}from"./index-fjceETmp.js";const c={__name:"Tasks",props:{server:{type:Object,required:!0}},setup(r){return(s,o)=>(t(),e("div",null," Tasks not yet implemented "))}};export{c as default};
+//# sourceMappingURL=Tasks-CGogD322.js.map
