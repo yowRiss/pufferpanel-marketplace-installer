85112473bb7407c441b3ff1c1692dd861a85117b
PufferPanel Marketplace Installer <installer@local>
2026-09-20 20:27:08 +0800

    fix(web): revert experimental chunk rename and restore standard index-7Al bundle

--- web/js/minecraft-java-D_cP_81x.js
+++ web/js/minecraft-java-D_cP_81x.js
@@ -1,2 +1,2 @@
-import r from"./generic-2Webhooks.js";import{f as t,o as a}from"./index-2Webhooks.js";import"./Tab-s1fK81I4.js";const p={__name:"minecraft-java",props:{server:{type:Object,required:!0}},setup(e){return(o,s)=>(a(),t(r,{server:e.server},null,8,["server"]))}};export{p as default};
+import r from"./generic-CCrhtAYP.js";import{f as t,o as a}from"./index-7Al--5D4.js";import"./Tab-s1fK81I4.js";const p={__name:"minecraft-java",props:{server:{type:Object,required:!0}},setup(e){return(o,s)=>(a(),t(r,{server:e.server},null,8,["server"]))}};export{p as default};
 //# sourceMappingURL=minecraft-java-D_cP_81x.js.map
